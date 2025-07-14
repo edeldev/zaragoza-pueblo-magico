d6ee0b50550f6c2b719f52dad7c26bf27712068c
edeldev <edeldev25@gmail.com>
2025-07-14 14:52:01 -0600

    feat: add gotas main apiClimate

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -48,6 +48,44 @@ export const Main = () => {
     }
   };
 
+  const renderRainDrops = () => {
+    if (weatherData?.icon === "rain") {
+      return (
+        <div className="absolute top-0 left-0 w-full h-full pointer-events-none z-10 overflow-hidden">
+          {[...Array(60)].map((_, index) => {
+            const left = Math.random() * 100;
+            const duration = 1 + Math.random() * 0.5;
+            const delay = Math.random() * 2;
+
+            return (
+              <motion.div
+                key={index}
+                className="absolute bg-white/40 rounded-full drop-shadow-lg"
+                style={{
+                  width: `${1 + Math.random() * 1.5}px`,
+                  height: `${10 + Math.random() * 10}px`,
+                  left: `${left}%`,
+                  top: `-100px`,
+                  opacity: Math.random() * 0.8 + 0.2,
+                }}
+                initial={{ y: -10 }}
+                animate={{ y: "110vh" }}
+                transition={{
+                  duration,
+                  delay,
+                  repeat: Infinity,
+                  ease: "linear",
+                }}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
+    return null;
+  };
+
   return (
     <div className="min-h-[400dvh] md:min-h-[300dvh] relative">
       <div className="min-h-screen flex flex-col justify-center gap-20 items-center py-10 text-center px-5">
@@ -104,6 +142,8 @@ export const Main = () => {
           </span>
         </div>
       </div>
+
+      {renderRainDrops()}
 
       <ModalVideo isModalOpen={isModalOpen} closeModal={closeModal} />
     </div>
