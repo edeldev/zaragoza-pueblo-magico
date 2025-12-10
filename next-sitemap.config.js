c2abdd42dec0bd8d611ca34226b4dd1ebfd9b192
edeldev <edeldev25@gmail.com>
2025-12-10 09:18:11 -0600

    site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.generalzaragoza.mx",
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+};
